e2906b72de08f6eaeae42cd6f487cf20d34143c6
Nishant <nishant301294@gmail.com>
2016-04-10 15:58:38 -0400

    Added the CSV parsing code

---
+++ data-import-code/movielensMovieImporter.py
@@ -0,0 +1,42 @@
+import os
+import re
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, Row
+
+# Regex used to seperate movie movieId, name, year, and genres
+RE = re.compile(r'(?P<movieId>\d+),"?(?P<name>.+)\((?P<year>\d+)\) ?"?,(?P<genres>.+)')
+sc = SparkContext("local", "MovielensMovieImporter") # Initialize the Spark context
+sqlContext = SQLContext(sc) # Initialize the SparkSQL context
+
+# Read in the text file as an RDD
+data = sc.textFile('../ml-latest-small/movies.csv')
+
+header = data.first() # Get the csv header
+data = data.filter(lambda line: line != header) # Filter out the csv header
+
+# Split the CSV file into rows
+# Formatter that takes the CSV line and outputs it as a list of datapoints
+# Uses a regex with named groups
+def formatter(line):
+    m = RE.match(line) # Seperates datapoints
+    if (m != None):
+        m = m.groupdict()
+        movieId = int(m['movieId'])
+        name = m['name']
+        year = int(m['year'])
+        genres = m['genres'].split('|')
+        return [movieId, name, year, genres]
+
+data = data.map(formatter)
+data = data.filter(lambda line: line != None) # Filter out rows that dont match
+
+# Test to make sure all the data is imported
+print data.count()
+
+# Map the data into a Row data object to prepare it for insertion
+rows = data.map(lambda r: Row(movieId=r[0], name=r[1], year=r[2], genres=r[3]))
+
+# Create the schema for movies and register a table for it
+schemaMovies = sqlContext.createDataFrame(rows)
+schemaMovies.registerTempTable("movies")
+schemaMovies.save('../tables/movies')
---
+++ data-import-code/movielensRatingImporter.py
@@ -0,0 +1,43 @@
+import os
+import re
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, Row
+
+SPARK_HOME = os.environ['SPARK_HOME']
+# Regex used to seperate movie movieId, name, year, and genres
+RE = re.compile(r'(?P<userId>\d+),(?P<movieId>\d+),(?P<rating>\d\.\d),(?P<timestamp>\d+)')
+sc = SparkContext("local", "MovielensLinkImporter") # Initialize the Spark context
+sqlContext = SQLContext(sc) # Initialize the SparkSQL context
+
+# Read in the text file as an RDD
+data = sc.textFile(SPARK_HOME + '/ml-latest-small/ratings.csv')
+
+header = data.first() # Get the csv header
+data = data.filter(lambda line: line != header) # Filter out the csv header
+
+# Split the CSV file into rows
+# Formatter that takes the CSV line and outputs it as a list of datapoints
+# Uses a regex with named groups
+def formatter(line):
+    m = RE.match(line) # Seperates datapoints
+    if (m != None):
+        m = m.groupdict()
+        userId = int(m['userId'])
+        movieId = int(m['movieId'])
+        rating = float(m['rating'])
+        timestamp = m['timestamp']
+        return [userId, movieId, rating, timestamp]
+
+data = data.map(formatter)
+data = data.filter(lambda line: line != None) # Filter out rows that dont match
+
+# Test to make sure all the data is imported
+print data.count()
+
+# Map the data into a Row data object to prepare it for insertion
+rows = data.map(lambda r: Row(userId=r[0], movieId=r[1], rating=r[2], timestamp=r[3]))
+
+# Create the schema for movies and register a table for it
+schemaRatings = sqlContext.createDataFrame(rows)
+schemaRatings.registerTempTable("ratings")
+schemaRatings.save('../tables/ratings')
