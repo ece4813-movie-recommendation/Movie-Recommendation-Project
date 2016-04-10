6aa1ef4d041a5f67c7a70fff0770ebebd1b815a7
Rohit Belapurkar <rohitrtb@gmail.com>
2016-04-10 14:08:40 -0400

    Script to install python recsys library. Rohit B

--- install_recsys.sh
+++ install_recsys.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Use this script to install the python recsys library in your local machine. 
+
 git clone https://github.com/ocelma/python-recsys.git
 tar xvzf python-recsys
 cd python-recsys
