7eefd131a6b4dbbc870799c407d4626687b76967
Nishant <nishant301294@gmail.com>
2016-04-19 22:13:00 -0400

    Frontend tweaks

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -8,18 +8,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   $('.search-icon').click(function() {
+    handleInput()
+  });
+
+  $('#input-box').keypress(function onEnter(e) {
+    if (e.which == 13) {
+      handleInput()
+    }
+  })
+
+  function handleInput() {
     var ip_box = $('#input-box');
     var input = ip_box.val(); // Get the input value
-    ip_box.val(''); // Clear the input
-    output = { 'data': input }
-    $.post('/data', JSON.stringify(output), function success(returnedData, status, resp) {
-      // Modify page
-      populateData(returnedData)
-      $('.loader').addClass('hidden')
-    }, 'json');
-    // Show loading
-    $('.loader').removeClass('hidden');
-  });
+    if (input != '') {
+      output = { 'data': input }
+      $.post('/data', JSON.stringify(output), function success(returnedData, status, resp) {
+        // Modify page
+        populateData(returnedData)
+        $('.loader').addClass('hidden')
+      }, 'json');
+      // Show loading
+      $('.loader').removeClass('hidden');
+    }
+  }
 
   function populateData(data) {
     var string_template = `
@@ -31,11 +42,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         <div class="panel-body">
           Extra Information
         </div>
+        <img src="http://dummyimage.com/80x120/000/fff">
       </div>
     </li>`
     $.each(eg, function(k_main, v_main) {
+      $('.lg-' + k_main).empty();
       $.each(eg[k_main], function(index, val) {
-        console.log(val)
         var res = string_template.replace('Movie Title', val)
         var res = res.replace('Extra Information', k_main)
         $('.lg-' + k_main).append(res);
