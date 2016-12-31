8464c0c588b7a48ea8da7d052e797ba7520e7aef
Baishen <baishen.huang@gmail.com>
2016-12-31 22:33:59 +0000

    no need to unzip a git clone repo

--- install_recsys.sh
+++ install_recsys.sh
@@ -3,6 +3,5 @@
 #Use this script to install the python recsys library in your local machine. 
 
 git clone https://github.com/ocelma/python-recsys.git
-tar xvzf python-recsys
 cd python-recsys
 sudo python setup.py install
